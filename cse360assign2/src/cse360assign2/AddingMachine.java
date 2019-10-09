2324a50b69f8eae8fa6712984f2c201d1d7d4891
Cooper <comcdona@asu.edu>
2019-10-08 21:12:57 -0700

    Added Javadoc comments

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,3 +1,12 @@
+/** 
+ * @author Cooper McDonald
+ * @author ID: 1214581524
+ * Assignment 2
+ *  This program creates an AddingMachine object that contains a int total. This class has methods
+ * that will manipulate this value such as getting, adding, subtracting, clearing, and setting to 
+ * a string.
+ */
+
 package cse360assign2;
 
 public class AddingMachine {
@@ -8,22 +17,40 @@ public class AddingMachine {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Returns the int total
+	 * @return total
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * Adds value to total
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * Subtracts value from total
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
-		
+	
+	/**
+	 * Converts total to a string and returns
+	 */
 	public String toString () {
 		return "";
 	}
 
+	/**
+	 * Clears the objects total value
+	 */
 	public void clear() {
 	
 	}
